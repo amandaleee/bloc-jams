6cb7c3633a55000e1fbaa9594823dc529913eae1
amandaleee <amandalee@amandalee.org>
2016-09-12 22:34:30 -0500

    updates to landing.js, refactor complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,14 @@
-var animatePoints = function() {
+function animatePoints() {
   var points = document.getElementsByClassName('point');
-  var pointsArray = Array.prototype.slice.call(points);
-  Array.from(document.getElementsByClassName("point")).forEach(function(item) {
-    item.style.opacity = 1;
-    item.style.transform = "scaleX(1) translateY(1rem)";
-    item.style.msTransform = "scaleX(1) translateY(1rem)";
-    item.style.WebkitTransform = "scaleX(1) translateY(1rem)";
-  });
+  var revealPoint = function(index){
+    points[index].style.opacity = 1;
+    points[index].style.transform = "scaleX(1) translateY(1rem)";
+    points[index].style.msTransform = "scaleX(1) translateY(1rem)";
+    points[index].style.WebkitTransform = "scaleX(1) translateY(1rem)";
+  }
+  for (var i = 0; i < points.length; i++) {
+    revealPoint(i);
+    console.log('revealPoint is working');
+  }
 };
-animatePoints();  
+animatePoints();
