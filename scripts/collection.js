ff04f50594bd23fc7dfeba034f700f52e5d0fc06
amandaleee <amandalee@amandalee.org>
2016-09-14 13:53:55 -0500

    spaces, no tabs

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,17 +1,17 @@
 var collectionItemTemplate = 
-+	'<div class="collection-album-container column fourth">' 
-+	' <img src="assets/images/album_covers/01.png"/>'
-+	' <div class="collection-album-info caption">'
-+	'   <p>'
-+	'      <a class="album-name" href="album.html">The Colors</a>'
-+	'      <br/>'
-+	'      <a href="#">Pablo Picasso</a>'
-+	'      <br/>'
-+	'      X songs'
-+	'      <br/>'
-+	'    </p>'
-+	'  </div><!--/collection-album-info-->'
-+	'</div><!--/collection-album-container-->';
+  +	'<div class="collection-album-container column fourth">' 
+  +	' <img src="assets/images/album_covers/01.png"/>'
+  +	' <div class="collection-album-info caption">'
+  +	'   <p>'
+  +	'      <a class="album-name" href="album.html">The Colors</a>'
+  +	'      <br/>'
+  +	'      <a href="#">Pablo Picasso</a>'
+  +	'      <br/>'
+  +	'      X songs'
+  +	'      <br/>'
+  +	'    </p>'
+  +	'  </div><!--/collection-album-info-->'
+  +	'</div><!--/collection-album-container-->';
 
 window.onload = function(){
 	var collectonContainer = document.getElementsByClassName('album-covers')[0];
