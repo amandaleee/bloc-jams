0048c393f7ba528f91c87eae86f066d46f0a2b9b
amandaleee <amandalee@amandalee.org>
2016-09-14 12:50:47 -0500

    animatePoint now uses my forEach instead of the baked-in forEach

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,16 +1,19 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var animatePoints = function(points) {
-  var revealPoint = function(index){
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(1rem)";
-    points[index].style.msTransform = "scaleX(1) translateY(1rem)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(1rem)";
-  };
-  for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  }
+var revealPoint = function(point){
+  point.style.opacity = 1;
+  point.style.transform = "scaleX(1) translateY(1rem)";
+  point.style.msTransform = "scaleX(1) translateY(1rem)";
+  point.style.WebkitTransform = "scaleX(1) translateY(1rem)";
 };
+
+var animatePoints = function(points) {
+  // for (var i = 0; i < points.length; i++) {
+  //   revealPoint(i);
+  // }
+  forEach(pointsArray, revealPoint);
+};
+
 window.onload = function() {
   if (window.innerHeight > 950) {
     animatePoints(pointsArray);
