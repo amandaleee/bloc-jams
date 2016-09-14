4e3275494b5f1b14bcf4076c2536bf5292c34ece
amandaleee <amandalee@amandalee.org>
2016-09-14 12:02:59 -0500

    screen size adjustment - run automatically on tall screens

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,4 +1,4 @@
-var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
   var revealPoint = function(index){
@@ -12,7 +12,14 @@ var animatePoints = function(points) {
   }
 };
 window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
-    console.log(event);
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);   
+    }
   });
 }
