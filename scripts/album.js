72c4703299fc50f93aa100778ffde7472d87e876
amandaleee <amandalee@amandalee.org>
2016-10-10 10:05:01 -0500

    some work

--- scripts/album.js
+++ scripts/album.js
@@ -30,7 +30,7 @@ var albumMarconi = {
 var createSongRow = function(songNumber, songName, songLength) {
   var template =
     '<tr class="album-view-song-item">'
-    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + </td>'
+    + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
     + '  <td class="song-item-title">' + songName + '</td>'
     + '  <td class="song-item-duration">' + songLength + '</td>'
     + '</tr>';
@@ -63,7 +63,10 @@ var setCurrentAlbum = function(album) {
 
 var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var songRows = document.getElementsByClassName('album-view-song-item');
+
+var currentlyPlayingSong = null;
 
 window.onload = function() {
   setCurrentAlbum(albumPicasso);
@@ -80,6 +83,48 @@ window.onload = function() {
       //this is why we have the data attr - we can still access it, even tho the innerhtml of the song number previously displayed on the page has turned into a play btn
       this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
     });
+    songRows[i].addEventListener('click', function(event) {
+      // Event handler call goes here
+      console.log("clicked");
+      clickHandler(event.target);
+    });
   }
+};
+var findParentByClassName = function(element, targetClass) {
+  if (element) {
+    var currentParent = element.parentElement;
+    while (currentParent.className != targetClass && currentParent.className !== null) {
+      currentParent = currentParent.parentElement;
+    }
+    return currentParent;
+  }
+};
 
+var getSongItem = function(element) {
+  switch (element.className) {
+    case 'album-song-button':
+    case 'ion-play':
+    case 'ion-pause':
+      return findParentByClassName(element, 'song-item-number');
+    case 'album-view-song-item':
+      return element.querySelector('.song-item-number');
+    case 'song-item-title':
+    case 'song-item-duration':
+      return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+    case 'song-item-number':
+      return element;
+    default:
+      return;
+  }  
+};
+
+var clickHandler = function(targetElement) {
+  var songItem = getSongItem(targetElement);
+  if (currentlyPlayingSong === null) {
+    songItem.innerHTML = pauseButtonTemplate;
+    currentlyPlayingSong = songItem.getAttribute('data-song-number');
+  } else if (currentlyPlayingSong ===songItem.getAttribute('data-song-number')){
+    songItem.innerHTML = playButtonTemplate;
+    currentlyPlayingSong = null;
+  }
 };
